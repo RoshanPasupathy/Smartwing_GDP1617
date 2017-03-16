bd2139fb4d66447e177273b2a8d452861ddcfad9
Roshan <roshan.pasupathy@gmail.com>
2017-03-16 11:35:36 +0000

    testing branching on matlab

--- Wind_tunnel_matlab/readme.m
+++ Wind_tunnel_matlab/readme.m
@@ -45,6 +45,7 @@ function readme()
 %                            - Waits for data to be available in the Serial buffer
 %                              before reading
 %  4. serial_close.m: Closes all open serial objects
+%  
 clc
 help readme
 end
